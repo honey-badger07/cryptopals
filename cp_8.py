a1aba5e5b7fe56cc7eed7385db70d76e6ab2cf67
honey <honey@badger-sec.com>
2023-02-20 17:53:45 -0700

    updates to ecb mode

--- cp_8.py
+++ cp_8.py
@@ -1,28 +1,30 @@
 from binascii import hexlify, unhexlify
 from Crypto.Cipher.AES import block_size
 
+
 def count_aes_ecb_repetitions(ciphertext):
     chunks = [ciphertext[i:i + block_size] for i in range(0, len(ciphertext), block_size)]
     number_of_duplicates = len(chunks) - len(set(chunks))
     return number_of_duplicates
 
 
-with open('data/input-4.txt') as f:
-    ctxts = [unhexlify(line.strip()) for line in f]
+def detect_ecb_encrypted_ciphertext(ciphertexts):
+    best = (-1,0)
 
-def has_repeated_blocks(ctxt, blocksize=16):
-    '''blocksize is in bytes'''
-    if len(ctxt) % blocksize != 0:
-        raise Exception('ciphertext length is not a multiple of block size')
-    else:
-        num_blocks = len(ctxt) // blocksize
-        
-    blocks = [ctxt[i*blocksize:(i+1)*blocksize] for i in range(num_blocks)]
-    
-    if len(set(blocks)) != num_blocks:
-        return True
-    else:
-        return False
+    for i in range(len(ciphertexts)):
+        repetitions = count_aes_ecb_repetitions(ciphertexts[i])
+        best = max(best, (i, repetitions), key=lambda t: t[1])
+    return best
 
-hits = [ctxt for ctxt in ctxts if has_repeated_blocks(ctxt)]
-print(hits)
+
+def main():
+    ciphertexts = [bytes.fromhex(line.strip()) for line in open('data/input-4.txt')]
+    result = detect_ecb_encrypted_ciphertext(ciphertexts)
+
+    print(f"The ciphertext encrypted in ECB mode is the one at position {result[0]} which contains {result[1]} repetitions")
+
+    assert result[0] == 132
+
+
+if __name__ == "__main__":
+    main()
